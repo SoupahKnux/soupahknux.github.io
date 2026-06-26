0a9990f137bf3737fa4253ff5f4903e10d4e18bf
SoupahKnux <superknuckles1306@gmail.com>
2026-06-26 19:28:13 +0700

    shoul work now methinks

--- faq/scripts.js
+++ faq/scripts.js
@@ -1,4 +1,4 @@
 let intervalId = setInterval(function(){
-    date = new Date()
-    document.getElementById("time").innerHTML = date.toLocaleString('ru-RU','Asia/Krasnoyarsk')
+    date = new Date();
+    document.getElementById("time").innerHTML = date.toLocaleString('ru-RU',{timeZone: "Asia/Krasnoyarsk"});
 },1000)
